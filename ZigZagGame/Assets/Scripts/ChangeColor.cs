521e83c44142fccb059f160bc088439dde702589
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2023-05-05 15:25:22 +0300

    Color changer end

--- ZigZagGame/Assets/Scripts/ChangeColor.cs
+++ ZigZagGame/Assets/Scripts/ChangeColor.cs
@@ -6,7 +6,7 @@ public class ChangeColor : MonoBehaviour
 {
     public Color[] colors;
 
-    Color ilkRenk, ikinciRenk;
+    Color ikinciRenk;
     int birinciRenk;
 
     public Material groundMat;
@@ -14,7 +14,35 @@ public class ChangeColor : MonoBehaviour
     private void Start()
     {
         birinciRenk = Random.Range(0, colors.Length);
+        ikinciRenk = colors[IkinciRengiSec()];
         groundMat.color = colors[birinciRenk];
         Camera.main.backgroundColor = colors[birinciRenk];
+    }
+
+    private void Update()
+    {
+        Color fark = groundMat.color - ikinciRenk;
+
+        if (Mathf.Abs(fark.r) + Mathf.Abs(fark.g) + Mathf.Abs(fark.b) < 0.2f)
+        {
+            ikinciRenk = colors[IkinciRengiSec()];
+        }
+        groundMat.color = Color.Lerp(groundMat.color,ikinciRenk,0.0003f);
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor,ikinciRenk,0.0007f);
+
+    }
+
+    int IkinciRengiSec()
+    {
+        int ikinciRenkIndex;
+        ikinciRenkIndex = Random.Range(0,colors.Length);
+
+
+        while (ikinciRenkIndex==birinciRenk)
+        {
+            ikinciRenkIndex = Random.Range(0, colors.Length);
+        }
+
+        return ikinciRenkIndex;
     }
 }
